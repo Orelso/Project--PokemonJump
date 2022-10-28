9ed0f49d02d3497478fbfcf0bca4701861ad5e90
Orelso Vitam <orelso.vitam@avanio.com>
2022-10-28 10:20:54 +0300

    2nd commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var character = document.getElementById("character");
-var block = document.getElementById("block");
+var pokemon = document.getElementById("pokemon");
 var flyingPokemon = document.getElementById("flyingPokemon")
 var counter = 0;
 var index = 0;
@@ -18,9 +18,10 @@ const images = [
   ];
 
   const flyingImages = [
-    'https://www.serebii.net/swordshield/pokemon/145.png',
+    
     'https://www.serebii.net/swordshield/pokemon/146.png',
-    "https://www.serebii.net/pokearth/sprites/pt/093.png"
+    'https://www.serebii.net/pokearth/sprites/pt/093.png',
+    'https://www.serebii.net/xy/pokemon/144.png'
 
   ]
   
@@ -29,23 +30,21 @@ const nextImage = () => images[Math.floor(Math.random() * images.length)];
 
 
 
-  function nextFlyingImage() {
-    if (index >= flyingImages.length) index = 0;
-    return flyingImages[index];
-  }
+ const nextFlyingImage =() => flyingImages[Math.floor(Math.random() * flyingImages.length)]; 
 
   function jump() {
     if (character.classList.contains("animate")) {
       return image[index]
     }
     
+ 
   
 
     character.classList.add("animate");
     setTimeout(function() {
       character.classList.remove("animate");
-      block.style.content = `url(${nextImage()})`;
-    //   flyingPokemon.style.content = `url(${nextFlyingImage()})`      
+      pokemon.style.content = `url(${nextImage()})`;
+      flyingPokemon.style.content = `url(${nextFlyingImage()})`      
     }, 300);
   }
 
@@ -54,13 +53,13 @@ const nextImage = () => images[Math.floor(Math.random() * images.length)];
 
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft < 20 && blockLeft >-20 && characterTop >= 130){
-        block.style.animation = "none";
+    let blockLeft = parseInt(window.getComputedStyle(pokemon).getPropertyValue("left"));
+    if(blockLeft < 70 && blockLeft > -70 && characterTop >= 260){
+        pokemon.style.animation = "none";
         alert("Game Over! Score: " + Math.floor(counter / 100));
         counter = 0;
-        block.style.animation = "block 1.3s infinite linear";
-        // flyingPokemon.style.animation = "block 3s infinite linear"
+        pokemon.style.animation = "block 1.3s infinite linear";
+        flyingPokemon.style.animation = "block 3s infinite linear"
 
     }else{
         counter++;
