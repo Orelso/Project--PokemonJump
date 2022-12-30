e50caecc380830be57f542b42e2849ec9b15f8c7
Orelso Vitam <orelso.vitam@avanio.com>
2022-12-30 14:12:41 +0200

    commit

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var flyingPokemon = document.getElementById("flyingPokemon")
 var counter = 0;
 var index = 0;
 
+
+document.body.onkeyup = function(e) {
+  if( e.keyCode === 32 ) {
+    jump();
+  }
+}
+
 const images = [
     'https://www.serebii.net/swordshield/pokemon/001.png',
     'https://www.serebii.net/swordshield/pokemon/002.png',
@@ -17,46 +24,41 @@ const images = [
 
   ];
 
+
   const flyingImages = [
     
     'https://www.serebii.net/swordshield/pokemon/146.png',
     'https://www.serebii.net/pokearth/sprites/pt/093.png',
-    'https://www.serebii.net/xy/pokemon/144.png'
+    'https://www.serebii.net/xy/pokemon/144.png',
+    'https://www.serebii.net/pokemon/art/145.png',
+    'https://www.serebii.net/sunmoon/pokemon/018.png',
+    'https://www.serebii.net/xy/pokemon/092.png'
+
 
   ]
   
-
 const nextImage = () => images[Math.floor(Math.random() * images.length)]; 
 
-
-
- const nextFlyingImage =() => flyingImages[Math.floor(Math.random() * flyingImages.length)]; 
+const nextFlyingImage =() => flyingImages[Math.floor(Math.random() * flyingImages.length)]; 
 
   function jump() {
-    if (character.classList.contains("animate")) {
-      return image[index]
-    }
-    
- 
-  
-
+    if (character.classList.contains("animate"))  {
+      return image[index]     
+    }    
     character.classList.add("animate");
     setTimeout(function() {
       character.classList.remove("animate");
       pokemon.style.content = `url(${nextImage()})`;
       flyingPokemon.style.content = `url(${nextFlyingImage()})`      
-    }, 300);
+    }, 1000);
   }
-
-
-
 
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(pokemon).getPropertyValue("left"));
-    if(blockLeft < 70 && blockLeft > -70 && characterTop >= 260){
+    if(blockLeft < 60 && blockLeft > -60 && characterTop >= 260){
         pokemon.style.animation = "none";
-        alert("Game Over! Score: " + Math.floor(counter / 100));
+        alert("GAME OVER! SCORE: "+ Math.floor(counter / 100) + "\n \nThe Pokemon will come slower now! Refresh the game for a FASTER challenge!");
         counter = 0;
         pokemon.style.animation = "block 1.3s infinite linear";
         flyingPokemon.style.animation = "block 3s infinite linear"
@@ -64,6 +66,7 @@ var checkDead = setInterval(function() {
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        // document.getElementById("imageSpan").innerHTML = pokemon.style.content = `url(${nextImage()})`;
     }
 }, 10);
 
